7cce8c9056efe4d0217d48d71ac954283eb9baa6
NikoAlyv <enihad801@gmail.com>
2024-01-28 07:12:17 +0400

    function task

---
+++ app.js
@@ -0,0 +1,53 @@
+//TODO: Task 1
+
+//! Part1
+// const numbers =[90,36,89];
+// function calculateSum(x,y,z){
+//     let sum =0
+    
+//     for(let i=0;i<numbers.length;i++){
+//          sum += numbers[i];
+      
+//     }
+//     return sum;
+// }
+
+// console.log(calculateSum(...numbers));
+
+
+//! Part2
+
+// let counter = 0;
+// while(counter<5){
+//     console.log("counter:",counter);
+//     counter++;
+// }
+
+//! Part3
+// let countDown = 5;
+
+// do{
+//     console.log("countDown",countDown);
+//     countDown--;
+
+// }while(countDown>0)
+
+//! Part4
+
+// function evaluateGrade(first,second,third,fourth,fifth){
+
+//     let i=0;
+//     while(i<arguments.length){ 
+
+//     if(arguments[i]>=60){
+//         console.log("Pass");
+//     }else{
+//         console.log("Fail");
+//     }
+
+//     i++;
+//     }
+
+// }
+
+// evaluateGrade(20,60,90,78,50)
